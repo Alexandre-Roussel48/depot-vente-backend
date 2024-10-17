77fc17b31c17460fcc6b2c860166f5337a942bee
= <alexandre.roussel.48@gmail.com>
2024-10-17 11:57:44 +0200

    add : app boilerplate

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { routes } = require('./routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(cookieParser());
+
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
+
+app.use(bodyParser.json());
+
+var expressWs = require('express-ws')(app);
+
+app.use('/api', routes());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
