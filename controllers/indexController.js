d081fbc70c11dea40a6d19678f468873e4a8d7cb
= <alexandre.roussel.48@gmail.com>
2024-10-31 09:01:14 +0100

    feat: catalog/session controllers -> index controller and implemented get current session/catalog

---
+++ controllers/indexController.js
@@ -0,0 +1,22 @@
+const { getSessionByDate } = require('../models/sessionModel');
+const { getRealGamesBySession } = require('../models/realGameModel');
+
+exports.getCurrentSession = async (req, res) => {
+  try {
+    const session = await getSessionByDate(new Date().toISOString());
+    res.status(200).json({ session: session });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getCurrentCatalog = async (req, res) => {
+  try {
+    const session = await getSessionByDate(new Date().toISOString());
+    const session_id = session.id;
+    const catalog = await getRealGamesBySession(session_id);
+    res.status(200).json({ catalog: catalog });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
